5a6370ac6eeaa853a01a2a74babcde953a62a15c
ashwini <ashwiniatole@gmail.com>
2023-07-04 17:17:24 +0530

    vb1

--- file1.js
+++ file1.js
@@ -1,6 +1,6 @@
 console.log("this is mai");
 console.log("this s b2");
-console.log("this s b3");
+console.log("this s b3 frpm master");
 
 
 
--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
 console.log("this is log file");
 console.log("this is second modified");
 console.log("this is next");
+console.log("this is b1 branch");
