e6e537ddf5e0dd9540f4b9be45d74283d25f8afc
ashwini <ashwiniatole@gmail.com>
2023-07-05 15:28:26 +0530

    this is commit

---
+++ file1.js
@@ -0,0 +1,12 @@
+console.log("this is mai");
+console.log("this s b2");
+console.log("this s b3 frpm master");
+console.log("this s b3 frpm b2");
+console.log("this is nb");
+console.log("conflitde");
+console.log("this is changed after github push");
+
+console.log("this is changed after github1 push");
+
+
+
